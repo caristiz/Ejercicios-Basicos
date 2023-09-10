c5f19996abd9c2d9d8f13608ebc79a5c48495525
Pipeline <catalinaaristizabal312613@correo.itm.edu.co>
2023-09-10 11:43:07 -0500

    Ejercio 2,3,4

--- ConceptosBasicos/Challenge.cs
+++ ConceptosBasicos/Challenge.cs
@@ -18,4 +18,60 @@ namespace itm.csharp.basic
 
         }
     }
+    
+    /* Solicita al usuario dos números. Si el primero es mayor, devuelva 
+    su doble, de lo contrario devuelva el triple del segundo.*/ 
+    public class Challenge2
+    {
+        public void Run()
+        {
+            int num1, num2;
+          Console.WriteLine("Ingrese un número");
+           int.TryParse(Console.ReadLine(), out num1);
+          Console.WriteLine("Ingrese otro número");
+           int.TryParse(Console.ReadLine(), out num2);
+
+           int resultado = num1 > num2 ? num1 * 2 : num2 * 3;
+
+          Console.WriteLine($"Resultado: {resultado}");
+        }
+    }
+
+    /*Pide al usuario un número. Si es positivo, devuelve su raíz
+    cuadrada, de lo contrario, devuelve su cuadrado */
+
+    public class Challenge3
+    {
+        public void Run()
+        {
+           Console.WriteLine("Digite un número");
+            double.TryParse(Console.ReadLine(), out double numero3);
+            if (numero3 > 0)
+            {
+                double resultado = Math.Sqrt(numero3);
+                Console.WriteLine ("La raiz cuadrad del número " + numero3 + " es " + resultado);
+            }
+            else if (numero3 <= 0)
+            {
+                double resultado = numero3*numero3;
+            }
+            else Console.WriteLine( "Ingresa un número válido.");
+
+        }
+    }
+
+    /*Pide al usuario el radio de un círculo y calcula su perímetro.*/
+
+    public class Challenge4
+    {
+        public void Run ()
+        {
+            Console.WriteLine("Ingrese el radio de la circunferencia");
+            double.TryParse(Console.ReadLine(), out double radio);
+            double perimetro = Math.PI * radio * 2;
+            Console.WriteLine( "El perímetro es: " + perimetro);
+        }
+    }
+
+
 }
