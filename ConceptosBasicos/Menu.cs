03d3fa863d92a9671872e5c28e600332500edac7
Pipeline <catalinaaristizabal312613@correo.itm.edu.co>
2023-09-10 10:34:52 -0500

    Menu

---
+++ ConceptosBasicos/Menu.cs
@@ -0,0 +1,24 @@
+namespace itm.csharp.basic
+{
+    public static class Menu
+    {
+        public static void MostrarMenu ()
+        {
+            Console.WriteLine("1.Positive Power");
+            Console.WriteLine("2.Double or triple");
+            Console.WriteLine("3.Root or Square");
+            Console.WriteLine("4.Circle Perimeter");
+            Console.WriteLine("5.Midweek Day");
+            Console.WriteLine("6.Tax Calculator");
+            Console.WriteLine("7.Remainder Finder");
+            Console.WriteLine("8.Sum of Evens");
+            Console.WriteLine("9.Fraccion Diferrence");
+            Console.WriteLine("10.String Length");
+            Console.WriteLine("11.Average of four");
+            Console.WriteLine("12.Smallest of Five");
+            Console.WriteLine("13.Vowel Counter");
+            Console.WriteLine("14.Factorial Finder");
+            Console.WriteLine("15.InRange Validator");
+        }
+    }
+}
